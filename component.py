bb0d44b83266ec40ab7af490f2e030fdda7f06e3
endlessproof <endlessproof@gmail.com>
2015-12-08 17:45:23 +0800

    MOD: modulation

--- component.py
+++ component.py
@@ -1,35 +1,35 @@
 class Customer:
 	def __init__(self, arrival_time, service_start_time, service_time):
-		self._arrival_time = arrival_time;
-		self._service_start_time = service_start_time;
-		self._service_time = service_time;
-		self._service_end_time = self.service_start_time + self.service_time;
-		self._wait = self.service_start_time + self.arrival_time;
+		self.__arrival_time = arrival_time;
+		self.__service_start_time = service_start_time;
+		self.__service_time = service_time;
+		self.__service_end_time = self.__service_start_time + self.__service_time;
+		self.__wait = self.__service_start_time + self.__arrival_time;
 
-		@property
-		def arrival_time(self):
-			return self._arrival_time;
-		@arrival_time.setter
-		def arrival_time(self, at):
-			self._arrival_time = at;
-		@property
-		def service_start_time(self):
-			return self._service_start_time;
-		@service_start_time.setter
-		def service_start_time(self, sst):
-			self._service_start_time = sst;
-		@property
-		def service_time(self):
-			return self._service_time;
-		@service_time.setter
-		def service_time(self, st):
-			self._service_time = st;
+	@property
+	def arrival_time(self):
+		return self.__arrival_time;
+	@arrival_time.setter
+	def arrival_time(self, at):
+		self.__arrival_time = at;
+	@property
+	def service_start_time(self):
+		return self.__service_start_time;
+	@service_start_time.setter
+	def service_start_time(self, sst):
+		self.__service_start_time = sst;
+	@property
+	def service_time(self):
+		return self.__service_time;
+	@service_time.setter
+	def service_time(self, st):
+		self.__service_time = st;
 
-		@property
-		def service_end_time(self):
-			return self._service_end_time;
-		@property
-		def wait(self):
-			return self._wait;
+	@property
+	def service_end_time(self):
+		return self.__service_start_time + self.__service_time;
+	@property
+	def wait(self):
+		return self.__wait;
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
