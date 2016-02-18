2644438f466ff9361040ca946a6fe056bc61ae10
endlessproof <endlessproof@gmail.com>
2016-02-19 01:41:53 +0800

    Modify: remove process property, refactor oop property and doc

--- lbps/device.py
+++ lbps/device.py
@@ -6,14 +6,13 @@ from poisson import Poisson
 class Device(object):
 	"""
 		property:
-		buf: buffer size
-		status: point out the device is on upstream downstream by 'U' or 'D'
-		process: using poisson only in this project
+		[protected]	buf: buffer size (bits)
+		[protected]	status: point out the device is on upstream downstream by 'U' or 'D'
+		[protected]	lambd: data rate (bps)
 	"""
-	def __init__(self):
-		self._buf = {'U':0, 'D':0};
-		self._status = 'D';
-		self._process = None;
+	# def __init__(self):
+	# 	self._buf = {'U':0, 'D':0};
+	# 	self._status = 'D';
 
 	@property
 	def buf(self):
@@ -23,79 +22,59 @@ class Device(object):
 		if dict.get('D') is None or dict.get('U') is None:
 			raise Exception("Buffer shold be a dict with pair ('D': [int]) and ('U': [int])");
 		self._buf = buf;
-	
+
 	@property
 	def status(self):
 		return self._status;
 	@status.setter
 	def status(self, status):
-		if status not 'D' or atatus not 'U':
+		if status not 'D' or status not 'U':
 			raise Exception("status value sould be 'D' or 'U'");
 		self._status = status;
-	
-	@property
-	def process(self):
-		return self._process;
-	@process.setter
-	def process(self, proc):
-		self._process = proc;
-	
-	"""
-	@property
-	def totalAwkTime(self):
-		return self.__totalAwkTime;
-	
-	@property
-	def delayBudget(self):
-		return self.__delayBudget;
 
+	# FIXME: need to consider more situation for data format and exception handling
 	@property
-	def pktArrive(self):
-		return self.__pktArrive;
-
-	@property
-	def pktDiscard(self):
-		return self.__pktDiscard;
-	"""
+	def lambd(self):
+		return self._lambd;
+	@lambd.setter
+	def lambd(self, lambd):
+		self._lambd = lambd;
 
 class UE(Device):
 	"""
 	@property
-	lambd: data rate
-	buf[2]: buffer size for upstream/downstream (from Device)
-	status: upstream/downstream (from Device)
-	process: binding Poisson (from Device)
+	[protected]	buf[2]
+	[protected]	status
+	[protected]	lambd
+	[private]	parentDevice: identity served RN
 	"""
-	# FIXME
-	def __init__(self, lambd, buf=[0,0], status='D'):
-		self.__lambd = lambd;
-		self._process = Poisson(self.__lambd);
+	def __init__(self, buf={'U':0, 'D':0}, status='D', lambd=None, parentDevice = None):
 		self._buf = buf;
 		self._status = status;
-		self.targetDevice = None;
-	
+		self._lambd = lambd;
+		self.__parentDevice = parentDevice;
+
+	# FIXME: need to consider more situation for data format and exception handling
 	@property
-	def lambd(self):
-		return self.__lambd;
-	@lambd.setter
-	def lambd(self, lambd):
-		self.__lambd = lambd;
+	def parentDevice(self):
+		return self.__parentDevice;
+	@parentDevice.setter(self, pD):	# should parent RN changable? or init in begining?
+		self.__parentDevice = pD;
 
 class RN(Device):
 	"""
 	@property
-	RUE: a list of served UE
-	lambd: aggregate the lambd from RUEs
-	buf[2]: buffer size for upstream/downstream (from Device)
-	status: upstream/downstream, could be different from UE (from Device)
-	process: binding Poisson (from Device)
+	[protected]	buf[2]
+	[protected]	status
+	[protected]	lambd
+	[private]	l_UE: a list of served UE
 	"""
 	# FIXME
-	def __init__(self, l_UE=None, buf={'D':0, 'U':0}, status='D'):
-		self.__l_UE = l_UE;
-		self.__lambd = None;
+	def __init__(self, buf={'U':0, 'D':0}, status='D', lambd=None, l_UE=None):
 		self._buf = buf;
 		self._status = status;
+		self._lambd = None;
+		self.__l_UE = l_UE;
 
 	@property
 	def l_UE(self):
@@ -107,6 +86,7 @@ class RN(Device):
 		self.__l_UE = l_UE;
 		setattr(self, '__lambd')
 
+	# rewrite lambd method, cause RN's lambd is equal to aggregate RUE lambd
 	@property
 	def lambd(self):
 		return self.__lambd;
@@ -116,12 +96,3 @@ class RN(Device):
 			print("There's no served UEs for aggregate lambd");
 		self.__lambd = sum(ue.lambd for ue in self.__l_UE);
 		setattr(self, '_process')
-	
-	@property
-	def process(self):
-		return self._process;
-	@process.setter
-	def process(self):
-		if self.__lambd is None:
-			print("There's no lambd for Poisson process");
-		self._process = Poisson(self.__lambd);
