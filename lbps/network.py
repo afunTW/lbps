124360084bd98f5eda84d774ba249c179a8af999
endlessproof <endlessproof@gmail.com>
2016-03-21 10:55:34 +0800

    add: network.py for Channel and packet module

---
+++ lbps/network.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
+class Channel(object):
+    def __init__(self, link='access', bandwidth=0, CQI=0):
+        """
+        property:
+        [protected] link: identify the link is 'backhaul' or 'access'
+        [protected] bandwidth: for calculating the capacity of the link (MHz)
+        [protected] CQI: identify the channel quality of the link
+        """
+        self._link = link;
+        self._bandwidth = bandwidth;
+        self._CQI = CQI;
