86aed51ddb86b119a4f3f340b161735ef2fb36be
endlessproof <endlessproof@gmail.com>
2016-03-24 21:10:37 +0800

    add: class Channel() with properties link, bandwidth, CQI

--- lbps/network.py
+++ lbps/network.py
@@ -12,3 +12,42 @@ class Channel(object):
         self._link = link;
         self._bandwidth = bandwidth;
         self._CQI = CQI;
+
+    @property
+    def link(self):
+        return self._link;
+    @link.setter
+    def link(self, link):
+        try:
+            if type(link) is str and (link.lower() is 'access' or link.lower() is 'backhaul'):
+                self._link = link;
+            else:
+                raise Exception("link should be str type and the value is 'access' or 'backhaul'")
+        except Exception as e:
+            print(e)
+
+    @property
+    def bandwidth(self):
+        return self._bandwidth;
+    @bandwidth.setter
+    def bandwidth(self, bw):
+        try:
+            if type(bw) is int:
+                self._bandwidth = bw;
+            else:
+                raise Exception("bandwidth should be the type of int (MHz)")
+        except Exception as e:
+            print(e);
+
+    @property
+    def CQI(self):
+        return self._CQI;
+    @CQI.setter
+    def CQI(self, CQI):
+        try:
+            if type(CQI) is int:
+                self._CQI = CQI;
+            else:
+                raise Exception("CQI should be the type of int")
+        except Exception as e:
+            print(e);
