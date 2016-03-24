aa97341c5400012c41770c645cabff1b75e07e8f
endlessproof <endlessproof@gmail.com>
2016-03-25 01:48:58 +0800

    add: __init__.py for env setting and const. var

--- lbps/__init__.py
+++ lbps/__init__.py
@@ -5,3 +5,36 @@ import random
 import copy
 from device import UE, RN, eNB
 from config import *
+
+"""
+Capacity
+# N_TTI_OFDM: number of OFDM symbol in one TTI
+# N_CTRL_OFDM: number of OFDM symbol in one TTI for signaling
+# RBG: one TTI conatin two RBs equal to one RBG
+# N_TTI_RE: number of RE in one TTI for data transmission
+
+Capacity = total REs * Eff(REs)
+
+Efficiency have to map to T_CQI by wideband or sub-band
+"""
+N_TTI_OFDM = 14
+N_CTRL_OFDM = 2
+N_RBG = 100
+N_S_RBG = 8
+
+# one TTI, one RBG
+N_TTI_RE = (N_TTI_OFDM-N_CTRL_OFDM)*12
+
+# one TTI, total RBGs = total REs
+N_TTI_RE = N_TTI_RE*N_RBG
+
+# # Capacity of wideband
+# def wideband_capacity(l_UE):
+#     """
+#     input a list of UE with bunch of different CQI,
+#     wideband will only calc by one avg. CQI
+#     """
+#     W_CQI = sum(l_UE.CQI)/len(l_UE);
+#     return N_TTI_RE*T_CQI[int(W_CQI)]*N_RBG;
+
+# Capacity of sub-band
--- lbps/config.py
+++ lbps/config.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
 
 # CQI = { CQI_index: modulation, code_rate*1024, efficiency }
-t_CQI = {
-        1: { 'modulation': 'QPSK', 'code-rate': 78, 'eff': 0.1523 },
-        2: { 'modulation': 'QPSK', 'code-rate': 120, 'eff': 0.2344 },
-        3: { 'modulation': 'QPSK', 'code-rate': 193, 'eff': 0.3770 },
-        4: { 'modulation': 'QPSK', 'code-rate': 308, 'eff': 0.6016 },
-        5: { 'modulation': 'QPSK', 'code-rate': 449, 'eff': 0.8770 },
-        6: { 'modulation': 'QPSK', 'code-rate': 602, 'eff': 1.1758 },
-        7: { 'modulation': '16QAM', 'code-rate': 378, 'eff': 1.4766 },
-        8: { 'modulation': '16QAM', 'code-rate': 490, 'eff': 1.9141 },
-        9: { 'modulation': '16QAM', 'code-rate': 616, 'eff': 2.4063 },
-        10: { 'modulation': '64QAM', 'code-rate': 466, 'eff': 2.7305 },
-        11: { 'modulation': '64QAM', 'code-rate': 567, 'eff': 3.3223 },
-        12: { 'modulation': '64QAM', 'code-rate': 666, 'eff': 3.9023 },
-        13: { 'modulation': '64QAM', 'code-rate': 772, 'eff': 4.5234 },
-        14: { 'modulation': '64QAM', 'code-rate': 873, 'eff': 5.1152 },
-        15: { 'modulation': '64QAM', 'code-rate': 948, 'eff': 5.5547 }
-    };
+T_CQI = {
+    1: { 'modulation': 'QPSK', 'code-rate': 78, 'eff': 0.1523 },
+    2: { 'modulation': 'QPSK', 'code-rate': 120, 'eff': 0.2344 },
+    3: { 'modulation': 'QPSK', 'code-rate': 193, 'eff': 0.3770 },
+    4: { 'modulation': 'QPSK', 'code-rate': 308, 'eff': 0.6016 },
+    5: { 'modulation': 'QPSK', 'code-rate': 449, 'eff': 0.8770 },
+    6: { 'modulation': 'QPSK', 'code-rate': 602, 'eff': 1.1758 },
+    7: { 'modulation': '16QAM', 'code-rate': 378, 'eff': 1.4766 },
+    8: { 'modulation': '16QAM', 'code-rate': 490, 'eff': 1.9141 },
+    9: { 'modulation': '16QAM', 'code-rate': 616, 'eff': 2.4063 },
+    10: { 'modulation': '64QAM', 'code-rate': 466, 'eff': 2.7305 },
+    11: { 'modulation': '64QAM', 'code-rate': 567, 'eff': 3.3223 },
+    12: { 'modulation': '64QAM', 'code-rate': 666, 'eff': 3.9023 },
+    13: { 'modulation': '64QAM', 'code-rate': 772, 'eff': 4.5234 },
+    14: { 'modulation': '64QAM', 'code-rate': 873, 'eff': 5.1152 },
+    15: { 'modulation': '64QAM', 'code-rate': 948, 'eff': 5.5547 }
+};
 
 # TDD_config = { index: [ DL/Special/UL subframe] }
-one_hop_TDD_config = {
-        0: ['D', 'S', 'U', 'U', 'U', 'D', 'S', 'U', 'U', 'U'],
-        1: ['D', 'S', 'U', 'U', 'D', 'D', 'S', 'U', 'U', 'D'],
-        2: ['D', 'S', 'U', 'D', 'D', 'D', 'S', 'U', 'D', 'D'],
-        3: ['D', 'S', 'U', 'U', 'U', 'D', 'D', 'D', 'D', 'D'],
-        4: ['D', 'S', 'U', 'U', 'D', 'D', 'D', 'D', 'D', 'D'],
-        5: ['D', 'S', 'U', 'D', 'D', 'D', 'D', 'D', 'D', 'D'],
-        6: ['D', 'S', 'U', 'U', 'U', 'D', 'S', 'U', 'U', 'D']
-    };
+ONE_HOP_TDD_CONFIG = {
+    0: ['D', 'S', 'U', 'U', 'U', 'D', 'S', 'U', 'U', 'U'],
+    1: ['D', 'S', 'U', 'U', 'D', 'D', 'S', 'U', 'U', 'D'],
+    2: ['D', 'S', 'U', 'D', 'D', 'D', 'S', 'U', 'D', 'D'],
+    3: ['D', 'S', 'U', 'U', 'U', 'D', 'D', 'D', 'D', 'D'],
+    4: ['D', 'S', 'U', 'U', 'D', 'D', 'D', 'D', 'D', 'D'],
+    5: ['D', 'S', 'U', 'D', 'D', 'D', 'D', 'D', 'D', 'D'],
+    6: ['D', 'S', 'U', 'U', 'U', 'D', 'S', 'U', 'U', 'D']
+};
 
-two_hop_TDD_config = {
+TWO_HOP_TDD_CONFIG = {
         0:[None, None, None, None, 'D', None, None, None, 'U', None],
         1:[None, None, None, 'U', None, None, None, None, None, 'D'],
         2:[None, None, None, None, 'D', None, None, None, 'U', 'D'],
@@ -50,4 +50,4 @@ two_hop_TDD_config = {
         16:[None, None, None, 'U', None, None, None, 'D', 'D', 'D'],
         17:[None, None, None, 'U', 'D', None, None, 'D', 'D', 'D'],
         18:[None, None, None, None, 'U', None, None, None, None, 'D']
-}
+};
