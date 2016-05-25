da25ba7be432ebd4b965ec53c64d98955ac88c08
endlessproof <endlessproof@gmail.com>
2016-05-26 00:24:05 +0800

    add: prompt

--- lbps/main.py
+++ lbps/main.py
@@ -5,7 +5,7 @@ from pprint import pprint
 
 NUMBER_OF_RN = 6
 NUMBER_OF_UE = 240
-ITERATE_TIMES = 12
+ITERATE_TIMES = 10
 SIMULATION_TIME = 1000
 PERFORMANCE = {
 	'LAMBDA':[],
@@ -84,6 +84,8 @@ for i in range(ITERATE_TIMES):
 		performance.update({\
 			rn.name:{
 			'PSE':0,
+			'sleep':0,
+			'awake':{'backhaul':0, 'access':0},
 			'force-awake':{'backhaul':0, 'access':0},
 			'waste-awake':{'backhaul':0, 'access':0},
 			'waste-cap':{'backhaul':0, 'access':0}
@@ -112,25 +114,25 @@ for i in range(ITERATE_TIMES):
 					continue
 
 				# lbps case
-				if rn in lbps['backhaul'][TTI] and rn in lbps['access'][TTI]:
-					interface = 'backhaul'
-					real_timeline_collision = True
-				elif rn in lbps['backhaul'][TTI]:
+				if rn in lbps['backhaul'][TTI]:
 					interface = 'backhaul'
 				elif rn in lbps['access'][TTI]:
 					interface = 'access'
 
 				# traffic stuck
-				if loading['backhaul'][rn.name] and base_station.tdd_config[TTI%10]:
+				if base_station.tdd_config[TTI%10] and loading['backhaul'][rn.name]:
+					performance[rn.name]['force-awake']['backhaul'] += 1 if not interface else 0
 					interface = 'backhaul'
 				elif loading['access'][rn.name]:
+					performance[rn.name]['force-awake']['access'] += 1 if not interface else 0
 					interface = 'access'
 
 				# backhaul transmission
 				if interface == 'backhaul':
-					if not base_station.queue[interface][rn.name] and real_timeline_collision:
+					if not base_station.queue[interface][rn.name] and rn in lbps['access'][TTI]:
 						interface = 'access'
 					else:
+						performance[rn.name]['awake']['backhaul'] += 1
 						available_cap = rn.capacity[interface]
 						for pkt in base_station.queue[interface][rn.name]:
 							if available_cap >= pkt['size']:
@@ -140,10 +142,13 @@ for i in range(ITERATE_TIMES):
 
 						for ue in rn.childs:
 							performance[ue.name]['PSE'] += 1/SIMULATION_TIME
-						# loading[interface][rn.name] = False if not base_station.queue[interface][rn.name] else True
+
+					loading[interface][rn.name] = False if not base_station.queue[interface][rn.name] else True
+					# performance[rn.name]['force-awake']['backhaul'] += 1 if not base_station.queue[interface][rn.name] else 0
 
 				# access transmission
 				if interface == 'access':
+					performance[rn.name]['awake']['access'] += 1
 					available_cap = rn.capacity[interface]
 					for pkt in rn.queue['backhaul']:
 						if available_cap >= pkt['size']:
@@ -151,13 +156,27 @@ for i in range(ITERATE_TIMES):
 							rn.queue['backhaul'].remove(pkt)
 							performance[pkt['device'].name]['delay'] += TTI-pkt['arrival_time']
 							available_cap -= pkt['size']
-					# loading[interface][rn.name] = False if not rn.queue['backhaul'] else True
+
+					loading[interface][rn.name] = False if not rn.queue['backhaul'] else True
+					# performance[rn.name]['force-awake']['access'] += 1 if not rn.queue['backhaul'] else 0
 
 				# sleep
 				if not interface:
+					performance[rn.name]['sleep'] += 1
 					performance[rn.name]['PSE'] += 1/SIMULATION_TIME
 					for ue in rn.childs:
 						performance[ue.name]['PSE'] += 1/SIMULATION_TIME
+
+		# test
+		for i in base_station.childs:
+			print(i.name, end='\t')
+			msg_execute("CQI= %d" % i.CQI, end='\t\t')
+			msg_warning("force awake in backhaul: %d times" % performance[i.name]['force-awake']['backhaul'], end='\t\t')
+			msg_warning("force awake in access: %d times" % performance[i.name]['force-awake']['access'])
+			# msg_execute("sleep: %d times" % performance[i.name]['sleep'], end='\t\t')
+			# msg_warning("awake in backhaul: %d times" % performance[i.name]['awake']['backhaul'], end='\t\t')
+			# msg_warning("awake in access: %d times" % performance[i.name]['awake']['access'], end='\t')
+			# msg_fail("%d" % (performance[i.name]['sleep']+performance[i.name]['awake']['backhaul']+performance[i.name]['awake']['access']))
 
 		# performance output
 		ue_name = [ue.name for rn in base_station.childs for ue in rn.childs]
