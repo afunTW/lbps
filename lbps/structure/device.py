8fcc8aaf9362860afbb8845de0dc9fe7be58ffcf
afunTW <endlessproof@gmail.com>
2017-03-19 11:49:20 +0800

    refactor: abstract device to onehop and two hop

--- lbps/structure/device.py
+++ lbps/structure/device.py
@@ -2,56 +2,126 @@ import sys
 import logging
 
 sys.path.append('../..')
-import definition
+from src import cqi
+from src import tdd
+from src import bearer
+from src import traffic
+from src import capacity
 
-# FIXME: keep buffer an bearer in type of list
-class LBPSDevice(object):
-    def __init__(self, name='device'):
+
+class OneHopDevice(object):
+    def __init__(self, name='one_hop_device'):
         self.name = name
-        self.buffer = {'backhaul': [], 'access': []}
-        self.bearer = {'backhaul': [], 'access': []}
+        self.buffer = []
+        self.bearer = []
         self.sleep = False
-        self.__interface = ''
-        self.__transmission = ''
+        self.__tdd_config = []
 
     @property
-    def interface(self): return self.__interface
-    @interface.setter
-    def interface(self, interface):
-        if isinstance(interface, str) and\
-        interface in ['backhaul', 'access']:
-            self.__interface = interface
+    def tdd_config(self):
+        return self.__tdd_config
 
-    @property
-    def transmission(self): return self.__transmission
-    @transmission.setter
-    def transmission(self, transmission):
-        if isintance(transmission, str) and\
-        transmission in ['downlink', 'uplink']:
-            self.__transmission = transmission
+    @tdd_config.setter
+    def tdd_config(self, conf):
+        try:
+            if instance(conf, int):
+                assert conf in tdd.one_hop_config.keys(),\
+                self.name + ' given index out of range'
+                self.__tdd_config = tdd.one_hop_config[conf]
+            elif isinstance(conf, list):
+                assert all(map(is_tdd_config, conf)),\
+                self.name + 'tdd config validation failed'
+                assert len(conf) == 10,\
+                self.name + ' given list length should be 10'
+                self.__tdd_config = conf
+            else:
+                raise Exception(
+                    self.name + ' given tdd config shuold be int either list'
+                )
+        except Exception as e:
+            logging.exception(e)
 
     @property
     def lambd(self):
-        # Bearer lambda aggregation
-        flow_map = {
-            'VoIP': traffic.VoIP(),
-            'Video': traffic.Video(),
-            'OnlineVideo': traffic.OnlineVideo()
-        }
+        try:
+            # Bearer lambda aggregation
+            flow_map = {
+                'VoIP': traffic.VoIP(),
+                'Video': traffic.Video(),
+                'OnlineVideo': traffic.OnlineVideo()
+            }
 
-        aggr_bh = [flow_map[b.flow].lambd for b in self.bearer['backhaul']]
-        aggr_ac = [flow_map[a.flow].lambd for a in self.bearer['access']]
-
-        return {
-            'backhaul': round(sum(aggr_bh), 2),
-            'access': round(sum(aggr_ac), 2)
-        }
+            aggr_lambd = [flow_map[b.flow].lambd for b in self.bearer]
+            return round(sum(aggr_lambd), 2)
+        except Exception as e:
+            logging.exception(e)
 
     @property
     def wideband_capacity(self):
-        if not self.__interface: return
-        if not self.bearer[self.__interface]: return
-        each_cqi = [bearer.CQI for bearer in self.bearer[self.__interface]]
-        each_eff = [cqi.cqi_info(i)['eff'] for i in each_cqi]
-        aggr_cap = sum([capacity.RE_TTI*eff for eff in each_eff])
-        return aggr_cap
+        try:
+            assert self.bearer, self.name + ' no bearer given'
+            each_cqi = [bearer.CQI for bearer in self.bearer]
+            each_eff = [cqi.cqi_info(i)['eff'] for i in each_cqi]
+            aggr_cap = sum([capacity.RE_TTI*eff for eff in each_eff])
+            return aggr_cap
+        except Exception as e:
+            logging.exception(e)
+
+    @property
+    def virtual_capacity(self):
+        try:
+            assert self.__tdd_config, self.name + ' tdd config is empty'
+            avaliable_frame_cap = \
+            self.__tdd_config.count('D') * self.wideband_capacity
+            return int(avaliable_frame_cap/10)
+        except Exception as e:
+            logging.exception(e)
+
+    @classmethod
+    def is_tdd_config(cls, conf):
+        try:
+            assert isinstance(conf, str),\
+            cls.name + ' tdd config validation with non-string'
+            assert len(conf) == 1,\
+            cls.name + ' tdd config validation over length'
+            return conf in ['D', 'U', '']
+        except Exception as e:
+            logging.exception(e)
+
+
+class TwoHopDevice(OneHopDevice):
+    def __init__(self, name='two_hop_device'):
+        self.name = name
+        self.backhaul = super().__init__(name=name)
+        self.access = super().__init__(name=name)
+        self.__tdd_config = None
+        self.__sleep = False
+
+    @property
+    def sleep(self):
+        return self.__sleep
+
+    @sleep.setter
+    def sleep(self, mode):
+        try:
+            assert isinstance(mode, bool), \
+            self.name + ' mode is not bool'
+            self.__sleep = mode
+            self.backhaul.sleep = mode
+            self.access.slepp = mode
+        except Exception as e:
+            raise e
+
+    @property
+    def tdd_config(self):
+        return self.__tdd_config
+
+    @tdd_config.setter
+    def tdd_config(self, index):
+        assert isinstance(index, int),\
+        self.name + ' given index is not int'
+        assert index in tdd.two_hop_config.keys(),\
+        self.name + ' given index out of range'
+        self.__tdd_config = two_hop_config[index]
+        self.backhaul.tdd_config = two_hop_config[index]['backhaul']
+        self.access.tdd_config = two_hop_config[index]['access']
