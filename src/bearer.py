8c4906a03710b445bb55b12cbff8756fb05e432b
afunTW <endlessproof@gmail.com>
2017-03-20 02:10:00 +0800

    modify: bearer handle connection rather than device

--- src/bearer.py
+++ src/bearer.py
@@ -8,15 +8,15 @@ from lbps.structure import device
 
 
 class Bearer(object):
-    def __init__(self, src, dest, CQI=0, flow=None):
+    def __init__(self, src, dest):
         assert isinstance(src, device.OneHopDevice) and\
         isinstance(dest, device.OneHopDevice),\
         'connection device is not the lbps.structure.device instances'
 
         self.__source = src
         self.__destination = dest
-        self.__CQI = CQI
-        self.__flow = flow
+        self.__CQI = 0
+        self.__flow = None
         self.__CQI_type = {
             'L': [1,2,3,4,5,6],
             'M': [7,8,9],
@@ -59,3 +59,12 @@ class Bearer(object):
             self.__flow = flow_type
         except Exception as e:
             logging.exception(e)
+
+    def build_connection(self):
+        assert self.__CQI > 0 and self.__CQI < 16,\
+        'please define CQI'
+        assert self.__flow,\
+        'please define flow'
+
+        self.__source.append_bearer(self)
+        self.__destination.append_bearer(self)
