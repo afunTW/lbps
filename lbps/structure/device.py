0dd0251f4d32ee043b2f93acde312bc31eb2fafa
afunTW <endlessproof@gmail.com>
2017-04-10 04:04:09 +0800

    modefied: throw exception insead of try except structure in class

--- lbps/structure/device.py
+++ lbps/structure/device.py
@@ -1,5 +1,4 @@
 import sys
-import logging
 
 sys.path.append('../..')
 from src import cqi
@@ -24,12 +23,9 @@ class OneHopDevice(object):
         return self.__bearer
 
     def append_bearer(self, b):
-        try:
-            assert isinstance(b, bearer.Bearer),\
-            'given data is not bearer.Bearer'
-            self.__bearer.append(b)
-        except Exception as e:
-            logging.exception(e)
+        assert isinstance(b, bearer.Bearer),\
+        'given data is not bearer.Bearer'
+        self.__bearer.append(b)
 
     def clear_bearer(self):
         self.__bearer = []
@@ -51,52 +47,40 @@ class OneHopDevice(object):
 
     @tdd_config.setter
     def tdd_config(self, conf):
-        try:
-            if instance(conf, int):
-                assert conf in tdd.one_hop_config.keys(),\
-                self.name + ' given index out of range'
-                self.__tdd_config = tdd.one_hop_config[conf]
-            elif isinstance(conf, list):
-                assert all(map(is_tdd_config, conf)),\
-                self.name + 'tdd config validation failed'
-                assert len(conf) == 10,\
-                self.name + ' given list length should be 10'
-                self.__tdd_config = conf
-            else:
-                raise Exception(
-                    self.name + ' given tdd config shuold be int either list'
-                )
-        except Exception as e:
-            logging.exception(e)
+        if instance(conf, int):
+            assert conf in tdd.one_hop_config.keys(),\
+            self.name + ' given index out of range'
+            self.__tdd_config = tdd.one_hop_config[conf]
+        elif isinstance(conf, list):
+            assert all(map(is_tdd_config, conf)),\
+            self.name + 'tdd config validation failed'
+            assert len(conf) == 10,\
+            self.name + ' given list length should be 10'
+            self.__tdd_config = conf
+        else:
+            raise Exception(
+                self.name + ' given tdd config shuold be int either list'
+            )
 
     @property
     def lambd(self):
-        try:
-            aggr_lambd = [b.flow.lambd for b in self.__bearer]
-            return round(sum(aggr_lambd), 2)
-        except Exception as e:
-            logging.exception(e)
+        aggr_lambd = [b.flow.lambd for b in self.__bearer]
+        return round(sum(aggr_lambd), 2)
 
     @property
     def wideband_capacity(self):
-        try:
-            assert self.__bearer, self.name + ' no bearer given'
-            each_cqi = [bearer.CQI for bearer in self.__bearer]
-            each_eff = [cqi.cqi_info(i)['eff'] for i in each_cqi]
-            aggr_cap = sum([capacity.RE_TTI*eff for eff in each_eff])
-            return aggr_cap
-        except Exception as e:
-            logging.exception(e)
+        assert self.__bearer, self.name + ' no bearer given'
+        each_cqi = [bearer.CQI for bearer in self.__bearer]
+        each_eff = [cqi.cqi_info(i)['eff'] for i in each_cqi]
+        aggr_cap = sum([capacity.RE_TTI*eff for eff in each_eff])
+        return aggr_cap
 
     @property
     def virtual_capacity(self):
-        try:
-            assert self.__tdd_config, self.name + ' tdd config is empty'
-            avaliable_frame_cap = \
-            self.__tdd_config.count('D') * self.wideband_capacity
-            return int(avaliable_frame_cap/10)
-        except Exception as e:
-            logging.exception(e)
+        assert self.__tdd_config, self.name + ' tdd config is empty'
+        avaliable_frame_cap = \
+        self.__tdd_config.count('D') * self.wideband_capacity
+        return int(avaliable_frame_cap/10)
 
     @property
     def lbps_capacity(self):
@@ -111,31 +95,25 @@ class OneHopDevice(object):
 
     @division_mode.setter
     def division_mode(self, mode):
-        try:
-            assert isinstance(mode, str), 'given mode is not string'
-            assert mode in ['FDD', 'TDD'], 'given mode is not FDD either TDD'
-            self.__division_mode = mode
-        except Exception as e:
-            raise e
+        assert isinstance(mode, str), 'given mode is not string'
+        assert mode in ['FDD', 'TDD'], 'given mode is not FDD either TDD'
+        self.__division_mode = mode
 
     @classmethod
     def is_tdd_config(cls, conf):
-        try:
-            assert isinstance(conf, str),\
-            cls.name + ' tdd config validation with non-string'
-            assert len(conf) == 1,\
-            cls.name + ' tdd config validation over length'
-            return conf in ['D', 'U', '']
-        except Exception as e:
-            logging.exception(e)
+        assert isinstance(conf, str),\
+        cls.name + ' tdd config validation with non-string'
+        assert len(conf) == 1,\
+        cls.name + ' tdd config validation over length'
+        return conf in ['D', 'U', '']
 
 
-class TwoHopDevice(OneHopDevice):
+class TwoHopDevice(object):
 
     def __init__(self, name=None):
         self.name = name or self.__class__.__name__
-        self.backhaul = super().__init__(name=name)
-        self.access = super().__init__(name=name)
+        self.backhaul = OneHopDevice(name=name)
+        self.access = OneHopDevice(name=name)
         self.__tdd_config = None
         self.__sleep = False
 
@@ -145,14 +123,12 @@ class TwoHopDevice(OneHopDevice):
 
     @sleep.setter
     def sleep(self, mode):
-        try:
-            assert isinstance(mode, bool), \
+        assert isinstance(mode, bool), (
             self.name + ' mode is not bool'
-            self.__sleep = mode
-            self.backhaul.sleep = mode
-            self.access.slepp = mode
-        except Exception as e:
-            raise e
+            )
+        self.__sleep = mode
+        self.backhaul.sleep = mode
+        self.access.slepp = mode
 
     @property
     def tdd_config(self):
@@ -160,10 +136,12 @@ class TwoHopDevice(OneHopDevice):
 
     @tdd_config.setter
     def tdd_config(self, index):
-        assert isinstance(index, int),\
-        self.name + ' given index is not int'
-        assert index in tdd.two_hop_config.keys(),\
-        self.name + ' given index out of range'
+        assert isinstance(index, int), (
+            self.name + ' given index is not int'
+            )
+        assert index in tdd.two_hop_config.keys(), (
+            self.name + ' given index out of range'
+            )
         self.__tdd_config = two_hop_config[index]
         self.backhaul.tdd_config = two_hop_config[index]['backhaul']
         self.access.tdd_config = two_hop_config[index]['access']
