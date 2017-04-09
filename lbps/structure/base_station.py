6b4950538f62fd626bfe5c6b7448c41d00d2a568
afunTW <endlessproof@gmail.com>
2017-04-10 04:34:49 +0800

    fixed: count the class instance in the right way

--- lbps/structure/base_station.py
+++ lbps/structure/base_station.py
@@ -1,12 +1,14 @@
 from . import device
+from itertools import count
+
 
 class BaseStation(device.OneHopDevice):
-    count = 0
+    count = count(0)
 
     def __init__(self, name=None):
         super(BaseStation, self).__init__()
         self.name = (
             name or '_'.join(
-                [self.__class__.__name__, str(self.__class__.count)]
+                [self.__class__.__name__, str(next(self.count))]
             )
         )
--- lbps/structure/relay_node.py
+++ lbps/structure/relay_node.py
@@ -1,12 +1,14 @@
 from . import device
+from itertools import count
+
 
 class RelayNode(device.TwoHopDevice):
-    count = 0
+    count = count(0)
 
     def __init__(self, name=None):
         self.name = (
             name or '_'.join(
-                [self.__class__.__name__, str(self.__class__.count)]
+                [self.__class__.__name__, str(next(self.count))]
             )
         )
         super(RelayNode, self).__init__(name=self.name)
