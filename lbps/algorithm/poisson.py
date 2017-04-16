1c764f9653db15063f4ee7630b350f05d05a7a76
afunTW <endlessproof@gmail.com>
2017-04-17 02:58:30 +0800

    fixed: calc sleep cycle logical error

--- lbps/algorithm/poisson.py
+++ lbps/algorithm/poisson.py
@@ -1,39 +1,43 @@
 from scipy.stats import poisson
 
 
-def prob_acc_under(lambd, threshold, sleep_cycle):
-    assert isinstance(threshold, float), 'please confirm the type of threshold'
-    prob_acc = 0
-
-    while threshold >= 0:
-        prob_acc += poisson.pmf(threshold, lambd*sleep_cycle)
-        threshold -= 1
-
-    return prob_acc
-
-def prob_acc_over(lambd, threshold, sleep_cycle):
-    prob = prob_acc_under(lambd, threshold, sleep_cycle)
-    return 1-prob if prob else 1
-
 def get_sleep_cycle(lambd, data_threshold, prob_threshold=0.8):
     sleep_cycle = 1
 
     while True:
-        prob_acc = prob_acc_over(lambd, data_threshold, sleep_cycle)
-        if prob_acc > prob_threshold:
+        mu = lambd * sleep_cycle
+        prob = [poisson.pmf(i, mu) for i in range(int(data_threshold))]
+        skip = False
+
+        for i, p in enumerate(prob):
+            if 1 - sum(prob[:i]) < prob_threshold:
+                skip = True
+                break
+
+        if skip:
+            sleep_cycle += 1
+        else:
             break
-        sleep_cycle += 1
 
     return sleep_cycle
 
+# FIXME: need test
 def get_data_accumulation(lambd, sleep_cycle, prob_threshold=0.8):
     packet = 0
 
     while True:
-        prob_acc = prob_acc_under(lambd, packet, sleep_cycle)
-        assert prob_acc
-        if prob_acc > prob_threshold:
+        mu = lambd * sleep_cycle
+        prob = [poisson.pmf(i, mu) for i in range(packet)]
+        skip = False
+
+        for i, p in enumerate(prob):
+            if sum(prob[:i]) > prob_threshold:
+                skip = True
+                break
+
+        if skip:
+            packet += 1
+        else:
             break
-        packet += 1
 
     return packet
