3e6ed7e1bba50b39a1e36b20b81585cd0505870b
afunTW <endlessproof@gmail.com>
2017-04-28 17:22:42 +0800

    rename: ignore

--- lbps/algorithm/basic.py
+++ lbps/algorithm/basic.py
@@ -2,13 +2,13 @@ import sys
 import logging
 
 sys.path.append('../..')
-from lbps.structure import device as dev
+from lbps.structure import device
 from lbps.algorithm import poisson
 
 
-def get_load(device):
-    avg_pkt_size = get_packet_size(device.bearer)
-    return device.lambd*avg_pkt_size/device.lbps_capacity
+def get_load(root):
+    avg_pkt_size = get_packet_size(root.bearer)
+    return root.lambd*avg_pkt_size/root.lbps_capacity
 
 def get_packet_size(bearer_buffer):
     total_pkt_size = sum([b.flow.packet_size for b in bearer_buffer])
@@ -17,28 +17,28 @@ def get_packet_size(bearer_buffer):
 def get_data_threshold(capacity, packet_size, percentage=0.8):
     return capacity/packet_size*percentage
 
-def aggr(device):
+def aggr(root):
     try:
         assert (
-            isinstance(device, dev.OneHopDevice) or
-            isinstance(device, dev.TwoHopDevice)
-            ), 'given device is not fit in lbps structure'
+            isinstance(root, device.OneHopDevice) or
+            isinstance(root, device.TwoHopDevice)
+            ), 'given root is not fit in lbps structure'
 
-        if isinstance(device, dev.TwoHopDevice):
-            device = device.access
+        if isinstance(root, device.TwoHopDevice):
+            root = root.access
 
-        capacity = device.lbps_capacity
-        packet_size = get_packet_size(device.bearer)
+        capacity = root.lbps_capacity
+        packet_size = get_packet_size(root.bearer)
         data_threshold = get_data_threshold(capacity, packet_size)
-        logging.info('running aggr under %f load' % get_load(device))
+        logging.info('running aggr under %f load' % get_load(root))
 
         # core
-        sleep_cycle = poisson.get_sleep_cycle(device.lambd, data_threshold)
-        logging.info('%s sleep cycle: %d' % (device.name, sleep_cycle))
+        sleep_cycle = poisson.get_sleep_cycle(root.lambd, data_threshold)
+        logging.info('%s sleep cycle: %d' % (root.name, sleep_cycle))
 
         timeline = [[] for i in range(sleep_cycle)]
-        timeline[0] = [i for i in device.target_device]
-        timeline[0].append(device)
+        timeline[0] = [i for i in root.target_device]
+        timeline[0].append(root)
         timeline[0] = sorted(timeline[0], key=lambda d: d.name)
         return timeline
 
