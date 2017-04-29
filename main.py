bc8c0fc6fafa433d029a3bbe740afab13f11c6bf
afunTW <endlessproof@gmail.com>
2017-04-29 14:25:17 +0800

    modified: change functionality in main()

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import lbps
 import logging
 
 from datetime import datetime
@@ -7,17 +8,13 @@ from lbps import network_tools as nt
 from lbps.algorithm import basic
 
 
-rn_count = 6
-ue_count = 240
-simulation_time = 10000
-
-def main():
-    global simulation_time
+def main(simulation_time):
+    # hot_spot_network = network_setup(15, 15, 96, 96, 12, 12, 12, 12)
     equal_load_network = nt.LBPSNetwork(15, 15, 40, 40, 40, 40, 40, 40)
     equal_load_network.set_tdd_configuration('two-hop', 17)
     equal_load_network.set_division_mode('TDD')
     equal_load_network.simulate(simulation_time)
-    # hot_spot_network = network_setup(15, 15, 96, 96, 12, 12, 12, 12)
+    result = equal_load_network.run(lbps.ALGORITHM_LBPS_AGGR)
 
 if __name__ == '__main__':
     now = datetime.now()
@@ -30,21 +27,24 @@ if __name__ == '__main__':
         logging.info('Creating directory %s' % logdir)
 
     logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s [%(levelname)8s] %(message)s',
+        level=logging.INFO,
+        format='%(asctime)s %(filename)s:L%(lineno)s [%(levelname)8s] %(message)s',
         datefmt='%b %d %H:%M:%S',
         filename=logname
         )
 
     stdout = logging.StreamHandler()
     stdout_format = logging.Formatter(
-        '%(asctime)s [%(levelname)8s] %(message)s',
+        '%(asctime)s %(filename)s:L%(lineno)s [%(levelname)8s] %(message)s',
         '%b %d %H:%M:%S')
     stdout.setFormatter(stdout_format)
     stdout.setLevel(logging.DEBUG)
     logging.getLogger().addHandler(stdout)
 
     try:
-        main()
+        rn_count = 6
+        ue_count = 240
+        simulation_time = 10000
+        main(simulation_time)
     except Exception as e:
         logging.exception(e)
