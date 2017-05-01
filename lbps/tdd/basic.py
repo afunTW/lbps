cad45acf2928b535a09a1a7c614888a12300be53
afunTW <endlessproof@gmail.com>
2017-05-01 22:07:37 +0800

    add: M1 one hop mapping

--- lbps/tdd/basic.py
+++ lbps/tdd/basic.py
@@ -6,25 +6,24 @@ from src import tdd as config
 
 
 class BaseMapping(object):
-    def __init__(self, tdd_config):
+    '''
+    Base Mapping class only handle one hop mapping
+    '''
+    def __init__(self, tdd_config, rsc=10):
 
         if isinstance(tdd_config, list):
             assert tdd_config in list(config.one_hop_config.values())
-        elif isinstance(tdd_config, dict):
-            assert tdd_config in list(config.two_hop_config.values())
         else:
             raise Exception('{} not the proper TDD config'.format(tdd_config))
 
         self.__tdd_config = tdd_config.copy()
-        self.__pattern = None
 
         # real/virtual subframe capacity
-        self.__rsc = 10
-        self.__vsc = tdd_config.count('D')*self.__rsc/10
-        self.__real_timeline = [self.__rsc for i in range(10)]
-        self.__virtual_timeline = [{
-            'r_TTI': [], 'vsc': self.__vsc
-            } for i in range(10)]
+        self.__rsc = None
+        self.__vsc = None
+        self.__real_timeline = None
+        self.__virtual_timeline = None
+        self.rsc = rsc
 
     @property
     def rsc(self):
@@ -35,7 +34,9 @@ class BaseMapping(object):
         assert isinstance(capacity, (int, float))
         self.__rsc = capacity
         self.vsc = self.__rsc
-        self.__real_timeline = [self.__rsc for i in range(10)]
+        self.__real_timeline = [{
+            'r_TTI': i, 'rsc': self.__rsc
+        } for i in range(10) if self.__tdd_config[i] is 'D']
 
     @property
     def vsc(self):
@@ -56,3 +57,26 @@ class BaseMapping(object):
     @property
     def virtual_timeline(self):
         return self.__virtual_timeline
+
+
+class one2all(BaseMapping):
+    '''
+    M1 mapping: one to all mapping
+    '''
+    def __init__(self, tdd_config):
+        super().__init__(tdd_config)
+        self.__pattern = None
+
+    @property
+    def pattern(self):
+        return self.__pattern
+
+    def run(self):
+        for rk, rv in enumerate(self.real_timeline):
+            allocate_rsc = rv['rsc']/10
+            for vk, vv in enumerate(self.virtual_timeline):
+                if vv['vsc'] >= allocate_rsc:
+                    vv['r_TTI'].append(rv['r_TTI'])
+                    vv['vsc'] -= allocate_rsc
+                    rv['rsc'] -= allocate_rsc
+        self.__pattern = [v['r_TTI'] for v in self.virtual_timeline]
